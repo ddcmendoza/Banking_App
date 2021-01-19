02a47c9b317c5c0e20f5d3a2e18559080125f68a
Dave Mendoza <ddcmendoza@gmail.com>
2021-01-20 00:40:08 +0800

    added currency formatter

--- view.js
+++ view.js
@@ -8,7 +8,11 @@ Repair Implementation so history will not be display here
 //div container for users
 const USERCONTAINER = document.getElementsByClassName("user-container")[0];
 
-
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'Php',
+    minimumFractionDigits: 2
+  })
 
 //Create child div containers for every item in the local storage, append to parent
 for (let i = 0; i < window.localStorage.length-1; i++) {
@@ -17,7 +21,7 @@ for (let i = 0; i < window.localStorage.length-1; i++) {
     let p = document.createElement("p");
     let obj = JSON.parse(localStorage[i]);
     h2.innerHTML = obj.user;
-    p.innerHTML = "Balance : Php " + obj.balance;
+    p.innerHTML = "Balance : " + formatter.format(obj.balance);
     div.appendChild(h2);
     div.appendChild(p);
     div.id = "userbox";
