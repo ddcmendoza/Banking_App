3d26448ae3c19b912b409d39b09a429f923be85c
Dave Mendoza <ddcmendoza@gmail.com>
2021-01-19 22:24:28 +0800

    history feature prepartion

--- create.js
+++ create.js
@@ -14,8 +14,8 @@ const ADDSCONTAINER = document.getElementsByClassName('adds-container')[0];
 const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 let users = [];
 
-for (let i = 0; i < window.localStorage.length; i++) {
-    let obj = JSON.parse(localStorage[window.localStorage.key(i)]);
+for (let i = 0; i < window.localStorage.length-1; i++) {
+    let obj = JSON.parse(localStorage[i]);
     users.push(obj.user);
 }
 console.log(users);
